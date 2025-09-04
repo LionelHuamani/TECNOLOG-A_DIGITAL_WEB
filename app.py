bb2df580bd8a695daf14d3fcb0d52a2ba65262d1
Juana Arenas <jarenas@vallegrande.edu.pe>
2025-09-04 10:02:17 -0500

    Guarda cambios antes de rebase

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #Lo que se va importar para el codigo
 from  flask  import  Flask, render_template
 #inicializar la aplicacion en flask
@@ -13,3 +14,22 @@ if __name__ == '__main__':
     app.run(debug=True)
 
 
+=======
+from flask import Flask, render_template ,request, jsonify,url_for
+import os
+
+# Inicializar de mi framework flask
+app = Flask(__name__)
+
+#rutas
+@app.route("/")
+def index():
+    ruta = os.path.join(app.static_folder, 'image')
+    imagenes = [url_for('static', filename=f'image/{img}') for img in os.listdir(ruta) if img.lower().endswith(('.png', '.jpg', '.jpeg', '.gif'))]
+    return render_template("index.html", imagenes=imagenes)
+
+#ejecutar mi servidor
+if __name__ == '__main__':
+    app.run(debug=True)
+
+>>>>>>> 154d9c7 (Primer commit)
--- static/js/carrusel.js
+++ static/js/carrusel.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const carousel = document.querySelector(".carousel-inner");
 const totalSlides = document.querySelectorAll(".carousel-item").length;
 let index = 0;
@@ -20,3 +21,30 @@ document.getElementById("next").addEventListener("click", nextSlide);
 document.getElementById("prev").addEventListener("click", prevSlide);
 
 setInterval(nextSlide, 3000); // Cambio automático cada 3 segundos
+=======
+const carousel = document.querySelector('.carousel-track');
+const dots = document.querySelectorAll('.carousel-indicator');
+
+const totalSlides = dots.length;
+let currentIndex = 0;
+
+function updateCarousel() {
+    carousel.style.transform = `translateX(-${currentIndex * 100}%)`;
+    dots.forEach((dot, i) => {
+        dot.classList.toggle('active', i === currentIndex);
+    });
+}
+
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % totalSlides;
+    updateCarousel();
+}
+
+function goToSlide(index) {
+    currentIndex = index;
+    updateCarousel();
+}
+
+updateCarousel();
+setInterval(nextSlide,3000); // Cambia de imagen cada 3 segundos
+>>>>>>> 154d9c7 (Primer commit)
